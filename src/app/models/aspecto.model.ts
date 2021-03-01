4b227c106638b283c86e72aa5baf3dedf391dfce
ivanmartinezv <ivmartez@gmail.com>
2021-03-01 02:19:38 -0300

    1 file modified, 1 added

---
+++ src/app/models/aspecto.model.ts
@@ -0,0 +1,34 @@
+export class Aspecto {
+  imagen: string;
+  nombre_aspecto: string;
+  tipo: string;
+  precio: number;
+  legado: boolean;
+  obtenible: boolean;
+  posesion: boolean;
+  botin: boolean;
+  id_campeon: number;
+
+  constructor() {}
+  /*constructor(
+    i: string,
+    na: string,
+    t: string,
+    p: number,
+    l: boolean,
+    o: boolean,
+    po: boolean,
+    b: boolean,
+    idc: number
+  ) {
+    this.imagen = i;
+    this.nombre_aspecto = na;
+    this.tipo = t;
+    this.precio = p;
+    this.legado = l;
+    this.obtenible = o;
+    this.posesion = po;
+    this.botin = b;
+    this.id_campeon = idc;
+  }*/
+}
